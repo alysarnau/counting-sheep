9d20f06dd243c8918371974bd3de1995677434f8
Alys <alice.g.cooper@gmail.com>
2022-06-14 13:03:34 -0400

    Add pseudocode to app.js, define player 1 and 2 objects

---
+++ app.js
@@ -0,0 +1,24 @@
+// premise of the game is you control a lil aussie dog that has to "collect" sheep
+// it's a time trial! P1 versus P2, fastest one wins!
+
+// need to define p1 object and p2 object
+const player1 = {
+    playername: '1',
+    points: 0,
+}
+
+const player2 = {
+    playername: '2',
+    points: 0,
+}
+
+// need to create a timer that counts up in seconds
+// need to display that timer onscreen
+
+// need to create sheep variable
+
+// when sheep is touched by the aussie avatar, +1 to player score
+
+// press space to bark?
+
+// include background music!
